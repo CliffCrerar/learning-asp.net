f84c68788241e46ea1dbe9031193f52d7f4dcead
cliffcrerar <cliff.crerar@gmail.com>
2020-06-06 01:03:30 +0200

    added models

--- ContosoUniversity/Models/Students.cs
+++ ContosoUniversity/Models/Students.cs
@@ -1,8 +1,14 @@
 using System;
+using System.Collections.Generic;
 
-namespace ContosoUniversity
+namespace ContosoUniversity.Models
 {
-    public class Students
+    public class Student
     {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
