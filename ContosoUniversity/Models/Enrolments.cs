5af76ae797854accd97bac6064e589d8b0405bbc
cliffcrerar <cliff.crerar@gmail.com>
2020-06-06 01:03:19 +0200

    added models

---
+++ ContosoUniversity/Models/Enrolments.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ContosoUniversity.Models
+{
+    public class Enrolments
+    {
+    }
+}
---
+++ ContosoUniversity/Models/Students.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ContosoUniversity
+{
+    public class Students
+    {
+    }
+}
