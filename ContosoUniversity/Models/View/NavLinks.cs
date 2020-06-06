373363576847425201c92947ab63b79ca0d7f0ab
cliffcrerar <cliff.crerar@gmail.com>
2020-06-06 18:44:56 +0200

    added seed data

---
+++ ContosoUniversity/Models/View/NavLinks.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models.View
+{
+    public class Link
+    {
+        public string LinkHref { get; set; }
+
+        public string LinkTitle { get; set; }
+
+        public Link(string title, string href)
+        {
+            this.LinkTitle = title;
+
+            this.LinkHref = href;
+        }
+    }
+
+    public class NavLinks
+    {
+        public List<Link> Links = new List<Link>();
+
+        public NavLinks()
+        {
+            Links.Add(new Link("About", "/About"));
+
+            Links.Add(new Link("Students", "/Students/Index"));
+
+            Links.Add(new Link("Courses", "/Courses/Index"));
+
+            Links.Add(new Link("Instructor", "/Instructor/Index"));
+
+            Links.Add(new Link("Departments", "/Departments/Index"));
+
+           
+        }
+    }
+}
---
+++ ContosoUniversity/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        public string Title = "Student Details";
+
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public DetailsModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Students { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Students = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Students == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
