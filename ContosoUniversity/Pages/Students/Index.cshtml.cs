e9d2dd616ff7c6baa3facd316c4c14244d47c5cd
cliffcrerar <cliff.crerar@gmail.com>
2020-06-07 20:45:54 +0200

    added pagination

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
-
+using Microsoft.AspNetCore;
 namespace ContosoUniversity.Pages.Students
 {
     public class IndexModel : PageModel
     {
+        int pageSize = 3;
+
         public string Title = "Students";
 
         private readonly ContosoUniversity.Data.SchoolContext _context;
@@ -28,23 +30,34 @@ namespace ContosoUniversity.Pages.Students
         public string DateSort { get; set; }
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
-        // public PaginatedList<Student> Students { get; set; }
+        public PaginatedList<Student> Students { get; set; }
 
-        public IList<Student> Students { get; set; }
+        //public IList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string searchString, string currentFilter, int? pageIndex)
         {
             NameSortArrow = "unsorted";
 
             DateSortArrow = "unsorted";
 
+            CurrentSort = sortOrder;
+
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
 
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
-            CurrentFilter = searchString;
-
             IQueryable<Student> studentsIQ = from s in _context.Students select s;
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
 
             if (!String.IsNullOrEmpty(searchString))
             {
@@ -71,7 +84,25 @@ namespace ContosoUniversity.Pages.Students
                     break;
             }
 
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+            Students = await PaginatedList<Student>.CreateAsync(studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+            //Students = await studentsIQ.AsNoTracking().ToListAsync();
+        }
+
+
+        public object SortArrowDisplay(string displayParam)
+        {
+            if (displayParam == "asc")
+            {
+                return "<i class='fas fa-arrow-up'></i>";
+            }
+            else if (displayParam == "des")
+            {
+                return "<i class='fas fa-arrow-down'></i>";
+            }
+            else
+            {
+                return "<i class='no-arrow'></i>";
+            }
         }
     }
 }
